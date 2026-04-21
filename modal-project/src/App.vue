a4e9e683574192e609e07cd6146e6045020c48f9
Ruben.Pulido1 <Ruben.Pulido1@co.kings.ca.us>
2026-04-21 11:42:48 -0700

    Creating SPA using Composition API

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,11 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import Modal from './components/Modal.vue';
+</script>
+
 
 <template>
   <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Modal/>
 </template>
 
-<style scoped></style>
+<style>
+  #app {
+    font-family: Avenir, Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  h1 {
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding-bottom: 10px;
+  }
+</style>
